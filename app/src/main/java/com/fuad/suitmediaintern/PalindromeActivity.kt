dc84189ce5295980c120667c81c3519c42661f5f
Fuad <fuadshidqi23@gmail.com>
2022-07-13 22:06:42 +0700

    add layout palindrome

--- app/src/main/java/com/fuad/suitmediaintern/PalindromeActivity.kt
+++ app/src/main/java/com/fuad/suitmediaintern/PalindromeActivity.kt
@@ -2,11 +2,16 @@ package com.fuad.suitmediaintern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.fuad.suitmediaintern.customview.NameEditText
+import com.fuad.suitmediaintern.databinding.ActivityPalindromeBinding
 
 class PalindromeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPalindromeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_palindrome)
+        binding = ActivityPalindromeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
     }
 }
